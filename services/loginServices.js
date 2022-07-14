6a611a2359efd33fe98c441a0dd53f37ce27a7ce
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-07-13 19:59:52 -0500

    hotfix: managing connection with backend

--- services/loginServices.js
+++ services/loginServices.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://sb-challenge-be.herokuapp.com';
 
 export const login = async (loginInfo) => {
   try {
--- services/userServices.js
+++ services/userServices.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8080';
+const API_URL = 'https://sb-challenge-be.herokuapp.com';
 
 export const createUser = async (payload) => {
   try {
